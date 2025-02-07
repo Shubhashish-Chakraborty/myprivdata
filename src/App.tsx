e12312a527b7a3cfcbf671dba174c68e6fba417e
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-02-07 22:30:51 +0530

    initial changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 export default function App() {
     return (
-        <>
-            <div className="bg-black text-red-400">
-                hello
+        <div className="h-screen text-custom-6 bg-custom-1">
+
+            <div className="flex justify-center mt-10">
+                <img
+                    src="myPrivData_Logo.png"
+                    className="w-28 text-white md:w-36 lg:w-48"
+                    alt="TheShortLinkLogo"
+                />
             </div>
-        </>
+
+            <div className="md:text-4xl text-xl cursor-progress hover:underline animate-bounce mt-28 transition-all duration-500 flex justify-center items-center">
+                Launching Veryy Soon!!
+            </div>
+            <div className="md:text-4xl text-xl cursor-progress hover:underline animate-bounce transition-all duration-500 flex justify-center items-center">
+                Working on It!!
+            </div>
+            <div className="flex justify-center gap-5 mt-10">
+                <div className="flex md:flex-row flex-col justify-center gap-5">
+                    <h1 className="hover:underline cursor-pointer" onClick={() => { window.open("https://github.com/Shubhashish-Chakraborty/myprivdata") }}>Frontend</h1>
+                    <h1 className="hover:underline cursor-pointer" onClick={() => { window.open("https://github.com/Shubhashish-Chakraborty/myprivdata-api") }}>Backend</h1>
+                </div>
+            </div>
+
+        </div>
     )
 }
